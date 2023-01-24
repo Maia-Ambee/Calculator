749fb7bdec769b529bb1479bdb4bc3c5e68fbc2f
Maia Ambee <m.ambartsumova@gmail.com>
2023-01-24 15:47:23 +0400

    Edit calc.py

--- calc.py
+++ calc.py
@@ -6,7 +6,4 @@ y = int(input("Y: "))
 print(f"Sum: {x + y}")
 print(f"Sub: {x - y}")
 print(f"Mul: {x * y}")
-if y == 0:	
-	print ("Can't do it"!)
-else:
-	print (f"Div: {x / y}")
+print(f"Div: {x / y}")
